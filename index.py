0723e2f1bdaeb0b48d9335a4545f0f2f540dc4b8
James Cuénod <j3frea@gmail.com>
2016-09-21 20:29:20 -0500

    Print out data in a table

--- index.py
+++ index.py
@@ -9,10 +9,10 @@ version='4b'
 API=fabric.load(source+version, 'lexicon', 'workshop', {
     "xmlids": {"node": False, "edge": False},
     "features": ('''
-        sp vs vt
+        sp nu gn ps vt vs st
         otype
         g_word lex_utf8
-        sp language gloss
+        language gloss
         chapter verse
     ''','mother'),
     "prepare": prepare,
@@ -21,16 +21,112 @@ API=fabric.load(source+version, 'lexicon', 'workshop', {
 exec(fabric.localnames.format(var='fabric'))
 
 
+sp_list = {
+	"art": "Article",
+	"verb": "Verb",
+	"subs": "Noun",
+	"nmpr": "Proper noun",
+	"advb": "Adverb",
+	"prep": "Preposition",
+	"conj": "Conjunction",
+	"prps": "Pers. pronoun",
+	"prde": "Demons. pron.",
+	"prin": "Interr. pronoun",
+	"intj": "Interjection",
+	"nega": "Negative",
+	"inrg": "Interrogative",
+	"adjv": "Adjective"
+}
+nu_list = {
+	"sg": "Singular",
+	"du": "Dual",
+	"pl": "Plural",
+	"unknown": "Unknown",
+	"NA": "NA"
+}
+gn_list = {
+	"m": "Masculine",
+	"f": "Feminine",
+	"unknown": "Unknown",
+	"NA": "NA"
+}
+ps_list = {
+	"p1": "First",
+	"p2": "Second",
+	"p3": "Third",
+	"unknown": "Unknown",
+	"NA": "NA"
+}
+vt_list = {
+	"perf": "Perfect",
+	"impf": "Imperfect",
+	"wayq": "Wayyiqtol",
+	"impv": "Imperative",
+	"infa": "Infinitive (Absolute)",
+	"infc": "Infinitive (Construct)",
+	"ptca": "Participle",
+	"ptcp": "Participle (Passive)",
+	"NA": "NA"
+}
+vs_list = {
+	"afel": "Af‘el",
+	"etpa": "Etpa“al",
+	"etpe": "Etpe‘el",
+	"haf": "Haf‘el",
+	"hif": "Hif‘il",
+	"hit": "Hitpa“el",
+	"hof": "Hof‘al",
+	"hop": "Hotpa“al",
+	"hsht": "Hishtaf‘al",
+	"htpa": "Hitpa“al",
+	"htpe": "Hitpe‘el",
+	"nif": "Nif‘al",
+	"nit": "Nitpa“el",
+	"pael": "Pa“el",
+	"pasq": "Passiveqal",
+	"peal": "Pe‘al",
+	"peil": "Pe‘il",
+	"piel": "Pi“el",
+	"pual": "Pu“al",
+	"qal": "Qal",
+	"shaf": "Shaf‘el",
+	"tif": "Tif‘al",
+	"NA": "NA"
+}
+st_list = {
+	"a": "Absolute",
+	"c": "Construct",
+	"e": "Emphatic",
+	"NA": "NA"
+}
+
+def remove_na(list_to_reduce):
+	templist = list_to_reduce
+	keys_to_remove = set()
+	for key, value in templist.items():
+		if value == "NA":
+			keys_to_remove.add(key)
+	print(keys_to_remove)
+	for key in keys_to_remove:
+		del templist[key]
+	return templist
+
+
 @route('/api/word_data/<node:int>')
 def api(node):
 	r = {
-		"node": node,
-		"sp": F.sp.v(node),
-		"vt": F.vt.v(node),
-		"vs": F.vs.v(node),
-		"gloss": F.gloss.v(node),
-		"lex": F.lex_utf8.v(node)
+		"Lexeme": F.g_lex_utf8.v(node),
+		"Part of Speech": sp_list[F.sp.v(node)],
+		"Person": ps_list[F.ps.v(node)],
+		"Number": nu_list[F.nu.v(node)],
+		"Gender": gn_list[F.gn.v(node)],
+		"Tense": vt_list[F.vt.v(node)], # vt = verbal tense
+		"Stem": vs_list[F.vs.v(node)], # vs = verbal stem
+		"State": st_list[F.st.v(node)], # construct/absolute/emphatic
+		# "Suffix": F.g_prs_utf8.v(node),
+		"Gloss": F.gloss.v(node)
 	}
+	r = remove_na(r);
 	return template('json', json=r)
 
 @route('/static/<filename>')
--- json.tpl
+++ json.tpl
@@ -1,1 +1,12 @@
-{{!json}}
+<table>
+	% for key, value in json.items():
+		<tr>
+			<td>
+				{{key}}
+			</td>
+			<td>
+				{{value}}
+			</td>
+		</tr>
+	% end
+</table>
