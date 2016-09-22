f2af022a23126dc4352ecc33d36722bf9513716d
James Cuénod <j3frea@gmail.com>
2016-09-21 19:05:26 -0500

    Show data on hover + darker highlight

--- static/lafwebpy.js
+++ static/lafwebpy.js
@@ -1,4 +1,4 @@
-$(document).on("click", "span", function(){
+$(document).on("mouseenter", "span", function(){
 	$.get("/api/word_data/" + $(this).data("node"), function(data){
 		$(".footer div").html(data);
 	});
