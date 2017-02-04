0819e2c6bd9fcf90373f2d9a88ef893c9e0b0600
James Cuénod <j3frea@gmail.com>
2017-02-03 19:46:45 -0600

    SBL Abbreviations that index.py uses

---
+++ book@sbl.tf
@@ -0,0 +1,48 @@
+@node
+@language=SBL Abbreviations
+@languageCode=sbl
+@languageEnglish=sbl
+@source=
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2016-01-26T07:05:47Z
+
+1367534	Gen
+Exod
+Lev
+Num
+Deut
+Josh
+Judg
+1 Sam
+2 Sam
+1 Kgs
+2 Kgs
+Isa
+Jer
+Ezek
+Hos
+Joel
+Amos
+Obad
+Jonah
+Mic
+Nah
+Hab
+Zeph
+Hag
+Zech
+Mal
+Ps
+Job
+Prov
+Ruth
+Song
+Eccl
+Lam
+Esth
+Dan
+Ezra
+Neh
+1 Chr
+2 Chr
